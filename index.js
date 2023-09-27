40ee6e0e45f353561fbdfe746525415acc091711
Irene Gitau <irene.gitau@strathmore.edu>
2023-09-27 16:11:50 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+const DODGER = document.getElementById('dodger');
+//moving the "DODGER"
+function moveDodgerLeft() {
+  const leftEdge = 0;
+  const currentPosition = parseInt(DODGER.style.left, 10);
+
+  if (currentPosition > leftEdge) {
+    DODGER.style.left = `${currentPosition - 4}px`;
+  }
+}
+//moving the "DODGER"
+function moveDodgerRight() {
+  const rightEdge = 360; 
+  const currentPosition = parseInt(DODGER.style.left, 10);
+
+  if (currentPosition < rightEdge) {
+    DODGER.style.left = `${currentPosition + 4}px`;
+  }
+}
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
